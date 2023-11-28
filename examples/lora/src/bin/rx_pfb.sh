8fc1b05200951490f5fbcfd3c20c549c44a10493
Vincenz Mechler <vmechler@seemoo.tu-darmstadt.de>
2023-11-28 18:11:11 +0100

    re-add lora polyphase-filterbank-channelizer example

---
+++ examples/lora/src/bin/rx_pfb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILD_TYPE=""
+#BUILD_TYPE="--release"
+CENTER_FREQ="--center-freq=867000000"
+RX_OFFSET="--rx-freq-offset=1.3e6"
+#TX_OFFSET="--tx-freq-offset=1.1e6"
+DEVICE_FILTER="--device-filter=driver=aaronia_http,tx_url=http://172.18.0.1:54665,url=http://172.18.0.1:54664"
+RX_GAIN="--rx-gain 10"
+#TX_DEVICE_FILTER="--device-filter=driver=soapy,soapy_driver=uhd,type=b200,name=B200mini"
+#TX_GAIN="--tx-gain 40"
+RX_ANTENNA=""
+#TX_ANTENNA=""
+SAMPLE_RATE="--sample-rate 600000"
+SPREADING_FACTOR="--spreading-factor 8"
+BANDWIDTH="--bandwidth 125000"
+
+export FUTURESDR_LOG_LEVEL=debug
+export RUST_BACKTRACE=full
+
+cargo run --bin rx_pfb ${BUILD_TYPE} -- ${SPREADING_FACTOR} ${BANDWIDTH} ${CENTER_FREQ} ${RX_OFFSET} ${DEVICE_FILTER} ${RX_GAIN} ${RX_ANTENNA} ${SAMPLE_RATE}
